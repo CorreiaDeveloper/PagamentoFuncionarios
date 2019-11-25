e998b83be22f5a266b3599636d904e4af5cd2eb4
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2019-11-24 21:41:59 -0300

    Iniciando Projeto de Pagamento de Funcionarios

---
+++ PagamentoFuncionarios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PagamentoFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
